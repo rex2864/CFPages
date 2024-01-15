8ce77eb225f6c2a5a62412f993a018540d81a2c8
rex2864 <rex2864@gmail.com>
2024-01-15 20:03:08 +0900

    listup python script updated

--- listup.py
+++ listup.py
@@ -1,1 +1,17 @@
-# listup python script
+#!/bin/python3
+
+import os
+
+files = os.listdir("posts/")
+files.sort(reverse=True)
+fp = open("posts.json", 'w')
+fp.write("{\"posts\":[")
+for f in files:
+    if f == "undefined.md" or f == "about.md":
+        continue
+    url = f[:-3]
+    date = f[:4] + "-" + f[4:6] + "-" + f[6:8]
+    title = f[10:-3].replace("_", " ")
+    fp.write("{\"url\":\"" + url + "\",\"date\":\"" + date + "\",\"title\":\"" + title + "\"},")
+fp.write("]}")
+fp.close()

0b1c49b79331dec9ace452d5e792b2c27aab68d7
rex2864 <rex2864@gmail.com>
2024-01-20 00:16:59 +0900

    new post added

--- listup.py
+++ listup.py
@@ -3,19 +3,22 @@ import os
 filelist = []
 for path, subdirs, files in os.walk("posts/"):
     for name in files:
+        if name == "undefined.md" or name == "about.md":
+            continue
         if name[-3:] == ".md":
             f = os.path.join(path, name)
             filelist.append(f[6:-3])
 filelist.sort(reverse=True)
 fp = open("posts.json", 'w')
-fp.write("{\"posts\":[")
+fp.write("{\n\t\"posts\":[\n")
 for f in filelist:
-    if f == "undefined" or f == "about":
-        continue
     fn = f.replace(os.path.sep, "")
     url = f
     date = fn[:4] + "-" + fn[4:6] + "-" + fn[6:8]
     title = fn[10:].replace("_", " ")
-    fp.write("{\"url\":\"" + url + "\",\"date\":\"" + date + "\",\"title\":\"" + title + "\"},")
-fp.write("]}")
+    fp.write("\t\t{\"url\":\"" + url + "\",\"date\":\"" + date + "\",\"title\":\"" + title + "\"}")
+    if filelist.index(f) < (len(filelist) - 1):
+        fp.write(",")
+    fp.write("\n")
+fp.write("\t]\n}\n")
 fp.close()
